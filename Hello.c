10f169d54cacbe3f25d20b4445f42b0e17a1d428
S1210353 <83548469+S1210353@users.noreply.github.com>
2021-05-02 22:15:34 +0530

    created hello.c
    It prints our name(Shilpa.R) 
     (Shilpa.R) loves to code ! 
    (Shilpa) also understands what i mean when i say that teir data type is human. Well, hopefully.

---
+++ Hello.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+  char name[30];
+  printf(" Enter your name :");
+  scanf("%s",name);
+  printf("%s loves to code !\n",name);
+  printf("%s also understands what i mean when i say that their data type is human. Well, hopefully.",name);
+  return 0;
+}
